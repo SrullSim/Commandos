29da78dfa2287395dc870d9864a4dbf6d86f659c
srullsim <simchonisrael@gmail.com>
2025-05-27 11:47:59 +0300

    add aircommando solider

---
+++ AirCommando.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class AirCommando: Commando
+    {
+        public string Parachute { get; set;}
+
+
+        // constructor
+        public AirCommando(string name , string codename, string[] tools, string parachute): base(name, codename, tools)
+        {
+            this.Parachute = parachute;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}

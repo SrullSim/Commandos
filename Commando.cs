f48044b1ec8cdaf528382740c28307d7faae34e1
srullsim <simchonisrael@gmail.com>
2025-05-27 09:47:42 +0300

    add Commando class

---
+++ Commando.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos
+{
+    internal class Commando
+    {
+
+        public string Name;
+        public string CodeName;
+        public string[] Tools;
+        public string Status;
+
+
+        // constractor
+        public void commando(string name, string codeName, string[] tools, string status)
+        {
+            this.Name = name;
+            this.CodeName = codeName;
+            this.Tools = tools;
+            this.Status = status;
+        }
+
+
+        // status walk 
+        public void Walk()
+        {
+            this.Status = "walking";
+            Console.WriteLine($"the commando {Name} now walking");
+        }
+
+
+        // status hide
+        public void Hide()
+        {
+            this.Status = "hiding";
+            Console.WriteLine($"the commando {Name} now hiding");
+        }
+
+
+        // status attack
+        public void Attack()
+        {
+            Console.WriteLine($"the commando {CodeName} now attacking");
+        }
+
+
+
+
+    }
+}
